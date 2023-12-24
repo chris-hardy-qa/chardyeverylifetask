3328725ab1aaa1ccc1504104266631c7fc4debab
Chris Hardy <chris.hardy@maistro.com>
2023-12-24 22:58:36 +0000

    implemented data file to store paramerised data to be used in tests

--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -5,6 +5,7 @@ const residentsPage = require('../pageobjects/residents.page')
 const newResidentPage = require('../pageobjects/new-resident.page')
 const residentProfilePage = require('../pageobjects/resident-profile.page')
 const detailsPage = require('../pageobjects/details.page')
+const testData = require('../testData/data')
 
 describe('My Login application', () => {
 
@@ -15,14 +16,14 @@ describe('My Login application', () => {
        
         await LoginPage.open()
 
-        await LoginPage.login('christopherHardy134679', '8eer&w1ne')
+        await LoginPage.login(testData.username, testData.password)
     })
 
     it('setup a new resident', async() => {
-        await expect(dashboardPage.navbar).toHaveText('christopherHardy134679')
+        await expect(dashboardPage.navbar).toHaveText(testData.username)
         await dashboardPage.goToResidentsPage();
         await residentsPage.addNewResident();
-        await newResidentPage.register('Mr', 'Test', 'Name', 'Testy', 'Male', '20/05/1987', '10', '80', '180', '20/05/2023', 'Cody Technology Park', 'Ively Rd', 'Farnborough ', 'Hampshire', 'GU14 0LX', 'UK', '0330 094 0122', 'test@everylifetechnologies.com', '07111 111111', 'Test', 'None', 'Someone', '07111 111112', 'Dr Man', 'The Surgery', '07111 111113');
+        await newResidentPage.register(testData.title, 'Test', 'Name', 'Testy', 'Male', '20/05/1987', '10', '80', '180', '20/05/2023', 'Cody Technology Park', 'Ively Rd', 'Farnborough ', 'Hampshire', 'GU14 0LX', 'UK', '0330 094 0122', 'test@everylifetechnologies.com', '07111 111111', 'Test', 'None', 'Someone', '07111 111112', 'Dr Man', 'The Surgery', '07111 111113');
         //await expect(residentProfilePage.checkHeader).toHaveText('Mr ' + 'Test' + ' Name');
         await residentProfilePage.openDetailsLink();
         await detailsPage.updateStatus('ACTIVE','Test Message');
